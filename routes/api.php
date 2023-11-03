1cbd8ffc6a713cd1dcff755981f5e278af39e63b
Leafarzzz <rafaeltolentino373@gmail.com>
2023-11-03 11:32:56 +0800

    created model file, controller file, added api crud routes, and added crud methods on controller

--- routes/api.php
+++ routes/api.php
@@ -10,22 +10,21 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-//get todos list
+// get todos list
 Route::get('/todos', [TodoController::class, 'index']);
-//retreive a single todo
+// retrieve a single todo
 Route::get('/todos/{id}', [TodoController::class, 'show']);
-//create a todo
+// create a todo
 Route::post('/todos', [TodoController::class, 'store']);
-//update a todo
+// update a todo
 Route::put('/todos/{id}', [TodoController::class, 'update']);
-//delete a todo
+// delete a todo
 Route::delete('/todos/{id}', [TodoController::class, 'destroy']);
